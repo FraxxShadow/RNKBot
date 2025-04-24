f0d9b9f85afdadd4f95ded8b42ec6048d711d8f3
Shadow <jujutsukaigojosaturo@gmail.com>
2025-04-24 15:00:49 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
     
         string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
         base64_string = await encode(string)
-        link = f"https://t.me/{client.username}?start={base64_string}"
+        link = f"https://t.me/{client.username}?start=filez{base64_string}"
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
         await second_message.reply_text(f"<b>Lɪɴᴋ - </b>{link}", quote=True, reply_markup=reply_markup)
 
@@ -61,6 +61,6 @@ async def link_generator(client: Client, message: Message):
     
         base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
         link = f"https://t.me/{client.username}?start={base64_string}"
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url=filez{link}')]])
         await channel_message.reply_text(f"<b>Lɪɴᴋ - </b>{link}", quote=True, reply_markup=reply_markup)
     
